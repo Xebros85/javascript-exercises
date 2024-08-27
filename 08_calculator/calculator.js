e1d5e56122430470936c34b7666b987c3792c6e1
Xebros85 <rorban1@live.com>
2024-08-26 21:42:14 -0500

    JS exercises 2

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let result = num1 + num2;
+  return result;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let result = num1 - num2;
+  return result;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // recursion
+	if (num < 0) {
+    return -1;
+  } else if (num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num - 1));
+  }
+
 };
 
 // Do not edit below this line
