ac57591ebfc169f29b31ca11bd6e4b07205cab8f
Xebros85 <rorban1@live.com>
2024-08-26 22:02:21 -0500

    JS exercises 3

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (prompt) {
+    let newPrompt = prompt.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    const len = newPrompt.length;
 
+    for (let i = 0; i < len / 2; i++) {
+        if (newPrompt[i] !== newPrompt[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
