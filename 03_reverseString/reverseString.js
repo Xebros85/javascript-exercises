cf04e2a6a186bbd164ffce61e5e61558ad3b566c
Xebros85 <rorban1@live.com>
2024-08-26 20:31:05 -0500

    exercises pt 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let result = '';
+    for (let i = words.length - 1; i >= 0; i--) {
+        result += words[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
